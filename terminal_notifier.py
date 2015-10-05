649f9bb76e2ebbc6db8a3203cc4db62c12f12584
Keith Smiley <keithbsmiley@gmail.com>
2015-10-04 23:13:00 -0700

    Add terminal notifier script

---
+++ terminal_notifier.py
@@ -0,0 +1,31 @@
+import distutils.spawn
+import os
+import pipes
+import weechat
+
+
+def notify(data, signal, signal_data):
+    command = ("terminal-notifier -message %s -title WeeChat -sound Hero"
+               % pipes.quote(signal_data))
+    exit_code = os.system(command)
+    if exit_code == 0:
+        return weechat.WEECHAT_RC_ERROR
+    else:
+        return weechat.WEECHAT_RC_OK
+
+
+def main():
+    if distutils.spawn.find_executable("terminal-notifier") is None:
+        return weechat.WEECHAT_RC_ERROR
+
+    if not weechat.register("terminal_notifier", "Keith Smiley", "1.0.0", "MIT",
+                            "Get OS X notifications for messages", "", ""):
+        return weechat.WEECHAT_RC_ERROR
+
+    weechat.hook_signal("weechat_pv", "notify", "")
+    weechat.hook_signal("weechat_highlight", "notify", "")
+
+    return weechat.WEECHAT_RC_OK
+
+if __name__ == "__main__":
+    main()
