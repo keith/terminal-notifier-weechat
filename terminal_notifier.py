af90ff01b1c375016986db0803b6202805b26bea
Keith Smiley <keithbsmiley@gmail.com>
2016-01-01 14:36:01 -0800

    Add notification throttling
    Now multiple notifications sent very quickly will be ignored

--- terminal_notifier.py
+++ terminal_notifier.py
@@ -7,17 +7,52 @@
 # Version 1.0.0: initial release
 # Version 1.0.1: fix escape characters which broke terminal-notifier
 # Version 1.0.2: set the nick as the title of the notification
+# Version 1.0.3: throttle notification calls
 
+
+import datetime
 import distutils.spawn
+import functools
 import os
+import os.path
 import pipes
 import weechat
+
+
+# https://gist.github.com/ChrisTM/5834503
+class throttle(object):
+    """
+    Decorator that prevents a function from being called more than once every
+    time period.
+
+    To create a function that cannot be called more than once a minute:
+
+        @throttle(minutes=1)
+        def my_fun():
+            pass
+    """
+    def __init__(self, seconds=0, minutes=0, hours=0):
+        self.throttle_period = datetime.timedelta(
+            seconds=seconds, minutes=minutes, hours=hours
+        )
+        self.time_of_last_call = datetime.datetime.min
+
+    def __call__(self, fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            now = datetime.datetime.now()
+            time_since_last_call = now - self.time_of_last_call
+            if time_since_last_call > self.throttle_period:
+                self.time_of_last_call = now
+                return fn(*args, **kwargs)
+        return wrapper
 
 
 def needs_escape(string):
     return string[0] in '[-("'
 
 
+@throttle(seconds=1)
 def notify(data, signal, signal_data):
     separated = signal_data.split("\t")
     try:
@@ -42,7 +77,7 @@ def main():
     if distutils.spawn.find_executable("terminal-notifier") is None:
         return weechat.WEECHAT_RC_ERROR
 
-    if not weechat.register("terminal_notifier", "Keith Smiley", "1.0.0", "MIT",
+    if not weechat.register("terminal_notifier", "Keith Smiley", "1.0.3", "MIT",
                             "Get OS X notifications for messages", "", ""):
         return weechat.WEECHAT_RC_ERROR
 
